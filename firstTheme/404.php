1a9797992940e89607031ee05b447dcf271b4cdf
sophiallen <smlelevich@gmail.com>
2016-12-11 16:12:52 -0800

    created a simple 404 page

---
+++ firstTheme/404.php
@@ -0,0 +1,13 @@
+<?php
+	get_header();
+	get_sidebar();
+?>
+
+<main>
+	<h2>Sorry, looks like this page doesn't exist yet!</h2>
+	<p>Click <a href="index.php">here</a> to return to the home page, or try searching for the page you wanted using the form below.</p>
+
+	<?php get_search_form(); ?>
+</main>
+
+<?php get_footer(); ?>	
